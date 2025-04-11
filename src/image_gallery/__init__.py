f496071692f4c589a72e805c1a0d55273aa9b022
Louke <git@jannsen.org>
2025-04-11 23:01:35 +0200

    Serve app using waitress

--- src/image_gallery/__init__.py
+++ src/image_gallery/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from flask import Flask
 from flask_injector import FlaskInjector
 
@@ -5,24 +8,19 @@ from image_gallery.configuration.config import GalleryConfig
 from image_gallery.configuration.modules import ServiceModule
 from image_gallery.controller import index, images, diashow
 
+Flask.url_for.__annotations__ = {}  # Workaround: https://github.com/python-injector/flask_injector/issues/78
 
-Flask.url_for.__annotations__ = {} # Workaround: https://github.com/python-injector/flask_injector/issues/78
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger('waitress')
+
 
 def create_app() -> Flask:
-    _app = Flask(__name__)
+    app = Flask(__name__)
+    app.config.from_object(GalleryConfig())
 
-    _app.config.from_object(GalleryConfig())
-    _app.config.from_prefixed_env()
+    app.register_blueprint(index.index_bp)
+    app.register_blueprint(images.images_bp)
+    app.register_blueprint(diashow.diashow_bp)
 
-    _app.register_blueprint(index.index_bp)
-    _app.register_blueprint(images.images_bp)
-    _app.register_blueprint(diashow.diashow_bp)
-
-    return _app
-
-def setup_injector(_app: Flask):
-    FlaskInjector(_app, modules=[ServiceModule])
-
-app = create_app()
-setup_injector(app)
-
+    FlaskInjector(app, modules=[ServiceModule])
+    return app
